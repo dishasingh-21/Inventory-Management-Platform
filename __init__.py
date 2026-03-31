fe1222bd75b05f5f9ad904e27ab7e59ec2c24b08
dishasingh-21 <disha210606@gmail.com>
2026-03-31 16:05:51 +0530

    Added inventory data (sales.csv,finished_goods.csv, raw_materials.csv, product_material_map.csv) in the data folder. Renamed app.py as __init__.py file (starts project everytime alongside initializing database), added db.py file (creates connection to sqlite database, initialises the database by executing the schema.sql file (data from csv files is written in our database)

---
+++ __init__.py
@@ -0,0 +1,79 @@
+import os
+from flask import Flask, render_template, request
+
+def create_app(test_config=None):
+    app=Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'inventory.sqlite'),
+    )
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import db
+    db.init_app(app)
+
+    return app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask,render_template,jsonify,request,g,current_app
+# import sqlite3
+
+# app = Flask(__name__)
+
+# def get_db():
+#     if 'conn' not in g:
+#         g.conn = sqlite3.connect("database.db")
+#         g.conn.row_factory=sqlite3.Row
+#     return g.conn
+
+# @app.teardown_appcontext
+# def close_db(exception):
+#     conn=g.pop('conn',None)
+#     if conn is not None:
+#         conn.close()
+
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+# @app.route("/dashboard")
+# def dashboard():
+#     return render_template("dashboard.html")
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
