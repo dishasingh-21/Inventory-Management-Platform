78bae9b4e8003ad709d104c3ee61730ff102dfd2
dishasingh-21 <disha210606@gmail.com>
2026-04-02 02:16:13 +0530

    changed import db line

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, request, jsonify
-from . import db
+import db
 import math
 def create_app(test_config=None):
     app=Flask(__name__, instance_relative_config=True)
